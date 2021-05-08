a591464651f48ecebcb8aab9f8f57f34dc300cdc
presi11 <juan.ospina19@udea.edu.co>
2021-05-07 20:46:55 -0500

    test env

--- src/HOC/Layout/LayoutHOC.jsx
+++ src/HOC/Layout/LayoutHOC.jsx
@@ -41,7 +41,7 @@ const LayoutHOC = ({ children }) => {
             className={LayoutStyles.cursorPointer}
             onClick={() => redirect("home")}
           >
-            Hermes
+            Hermes 2
           </MDBNavbarBrand>
           <MDBNavbarToggler
             type="button"
