9deca53fbe4bfc8dc9fa3c494f9f1f0a3aca4566
presi11 <juan.ospina19@udea.edu.co>
2021-05-13 18:59:03 -0500

    add card order

--- src/Components/OrderCard/OrderCard.jsx
+++ src/Components/OrderCard/OrderCard.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
+import {
+    MDBCard,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBListGroupItem,
+    MDBCardHeader,
+    MDBTabs,
+    MDBTabsItem,
+    MDBTabsLink,
+    MDBListGroup,
+  } from "mdb-react-ui-kit";
+ 
+// function isactive() {
+
+// }  
 
 const OrderCard = () => {
     return (
-        <div>
-            This is OrderCard
-        </div>
+        <>
+        <MDBCard className="text-center">
+          <MDBCardHeader>
+            <MDBTabs pills className="card-header-tabs">
+              <MDBTabsItem>
+                <MDBTabsLink active>Pendientes</MDBTabsLink>
+              </MDBTabsItem>
+              <MDBTabsItem>
+                <MDBTabsLink> Historial </MDBTabsLink>
+              </MDBTabsItem>
+            </MDBTabs>
+          </MDBCardHeader>
+          <MDBCardBody>
+            <MDBCardTitle>Estos son sus pedidos pendientes</MDBCardTitle>
+            <MDBListGroup flush>
+              <MDBListGroupItem></MDBListGroupItem>
+            </MDBListGroup>
+          </MDBCardBody>
+        </MDBCard>
+      </>
     )
 }
 
--- src/Pages/Orders/Orders.jsx
+++ src/Pages/Orders/Orders.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React from "react";
 import { useQuery } from "@apollo/client";
 import { ORDERS } from "../../graphql/queries";
+import OrderCard from "../../Components/OrderCard/OrderCard"
 
 const Orders = () => {
-    const { loading, error, data } = useQuery(ORDERS);
-    if (loading) return 'Loading...';
-    if (error) return `Error! ${ error.message }`;
+  const { loading, error, data } = useQuery(ORDERS);
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
+  data.orders.map((order, index) => {
+    return <p key={index}>{JSON.stringify(order)}</p>;
+  });
 
-    return (
-        data.orders.map((order, index) => {
-            return <p key={ index }>{ JSON.stringify(order) }</p>
-        })
-    )
-}
+
+  return (
+    <OrderCard></OrderCard>
+  );
+};
 
 export default Orders;
